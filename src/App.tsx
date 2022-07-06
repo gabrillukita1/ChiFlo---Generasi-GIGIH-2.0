4ed842ff20b0e25d4e26c06a159f8d6aff4b68d0
tariraf <tarisarafika2@gmail.com>
2022-07-06 19:55:13 +0700

    -

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { Route, Routes } from "react-router-dom"
+import Cleaning from "./pages/Cleaning"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+
+function App() {
+  return (
+    <>
+      {/* <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes> */}
+
+      <Cleaning />
+    </>
+  )
+}
+
+export default App
