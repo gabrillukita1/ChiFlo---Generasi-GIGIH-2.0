6eb07e07f541fc86dec5826242ee0b9b2b050dd0
tariraf <tarisarafika2@gmail.com>
2022-07-11 21:38:51 +0700

    -

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import { Route, Routes } from "react-router-dom"
+<<<<<<< Updated upstream
 import Cleaning from "./pages/Cleaning"
 import Home from "./pages/Home"
 import Insurance from "./pages/Insurance"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
 
+=======
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import CleaningOverview from "./components/CleaningOverview"
+import Cleaning from "./pages/Cleaning"
+import CleaningService from "./components/CleaningService"
+>>>>>>> Stashed changes
 
 function App() {
   return (
     <>
+<<<<<<< Updated upstream
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
@@ -17,6 +27,14 @@ function App() {
         <Route path="/insurance" element={<Insurance />} />
       </Routes>
 
+=======
+      {/* <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes> */}
+      <Cleaning />
+>>>>>>> Stashed changes
     </>
   )
 }
